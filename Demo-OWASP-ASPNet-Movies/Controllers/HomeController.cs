b0eec26de03cb525a7b4f101991698f16ca6b96d
JJ Orozco López <jjorozcodev@gmail.com>
2019-10-26 05:55:45 -0600

    Busqueda vulnerable de SQLinjection
    Usando ADO.Net y exponiendo la vulnerabilidad en un filtro de búsqueda.

--- Demo-OWASP-ASPNet-Movies/Controllers/HomeController.cs
+++ Demo-OWASP-ASPNet-Movies/Controllers/HomeController.cs
@@ -1,13 +1,66 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Demo_OWASP_ASPNet_Movies.Controllers
 {
     public class HomeController : Controller
     {
+        string connectionString = "Server=localhost;Database=MoviesDB;User Id=sa; Password=masterKey;";
+
+        Models.MoviesDBEntities db = new Models.MoviesDBEntities();
+
         public ActionResult Index()
         {
-            return View();
+            List<Models.MovieView> myMovies = new List<Models.MovieView>();
+
+            if (Session["logged"] == null || (Convert.ToInt32(Session["logged"].ToString()) != 1))
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                if(TempData["searchResult"] != null)
+                {
+                    return View(TempData["searchResult"]);
+                }
+
+                int idU = Convert.ToInt32(Session["userId"].ToString());
+
+                string sql = "select distinct M.idMovie, C.category, M.title, M.release ";
+                sql += "from Movies M inner join Categories C on M.idCategory = C.idCategory ";
+                sql += "inner join Subscriptions S on C.idCategory = S.idCategory ";
+                sql += "where S.idUser = " + idU;
+                
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Models.MovieView mv = new Models.MovieView();
+                            mv.IdMovie = Convert.ToInt32(reader[0].ToString());
+                            mv.Category = reader[1].ToString();
+                            mv.Title = reader[2].ToString();
+                            mv.ReleaseYear = Convert.ToInt32(reader[3].ToString());
+
+                            myMovies.Add(mv);
+                        }
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.Message);
+                    }
+                }
+            }
+            
+            return View(myMovies);
         }
 
         public ActionResult Login()
@@ -18,16 +71,59 @@ namespace Demo_OWASP_ASPNet_Movies.Controllers
         [HttpPost]
         public ActionResult Login(string username, string password)
         {
-            Models.MoviesDBEntities db = new Models.MoviesDBEntities();
-
             Models.Users currentUser = db.Users1.Where(u => u.username == username & u.password == password).FirstOrDefault();
 
             if(currentUser != null)
             {
+                Session["userId"] = currentUser.idUser;
+                Session["logged"] = 1;
                 return RedirectToAction("Index");
             }
 
             return RedirectToAction("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Search(string criteria)
+        {
+            List<Models.MovieView> myMovies = new List<Models.MovieView>();
+
+
+                int idU = Convert.ToInt32(Session["userId"].ToString());
+
+                string sql = "select distinct M.idMovie, C.category, M.title, M.release ";
+                sql += "from Movies M inner join Categories C on M.idCategory = C.idCategory ";
+                sql += "inner join Subscriptions S on C.idCategory = S.idCategory ";
+                sql += "where S.idUser = " + idU + " and M.title like '%" + criteria + "%'";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Models.MovieView mv = new Models.MovieView();
+                            mv.IdMovie = Convert.ToInt32(reader[0].ToString());
+                            mv.Category = reader[1].ToString();
+                            mv.Title = reader[2].ToString();
+                            mv.ReleaseYear = Convert.ToInt32(reader[3].ToString());
+
+                            myMovies.Add(mv);
+                        }
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.Message);
+                    }
+            }
+
+            TempData["searchResult"] = myMovies;
+
+            return RedirectToAction("Index");
         }
 
 
---
+++ Demo-OWASP-ASPNet-Movies/Models/MovieView.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_OWASP_ASPNet_Movies.Models
+{
+    public class MovieView
+    {
+        public int IdMovie { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
